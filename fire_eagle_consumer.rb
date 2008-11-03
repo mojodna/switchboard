644d5fedba895b5d375b25b2ff11914b1b2cf772
Seth Fitzsimmons <seth@mojodna.net>
2008-11-02 22:41:24 -0800

    preliminary fire eagle consumer

---
+++ fire_eagle_consumer.rb
@@ -0,0 +1,24 @@
+#!/usr/bin/env ruby
+require 'fire_hydrant'
+require 'fire_hydrant/jacks/debug_jack'
+require 'fire_hydrant/jacks/oauth_pubsub_jack'
+require 'fireeagle'
+
+hydrant = FireHydrant.new(YAML.load(File.read("fire_hydrant.yml")), true)
+hydrant.jack!(OAuthPubSubJack)
+
+hydrant.on_pubsub_event do |event|
+  event.payload.each do |payload|
+    puts "Node: #{payload.node}"
+    payload.elements.each do |item|
+      rsp = item.first_element("rsp")
+      response = FireEagle::Response.new(rsp.to_s)
+      user = response.users[0]
+      # for some reason the Fire Eagle gem doesn't let me do this
+      name = user.best_guess.instance_eval { @doc.at("//name").innerText }
+      puts "#{user.token} has moved to #{name}."
+    end
+  end
+end
+
+hydrant.run!
