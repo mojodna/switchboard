06c8a65ea7b0ef571f81b066a5668bf4cb0c45b7
Seth Fitzsimmons <seth@mojodna.net>
2008-11-02 22:40:57 -0800

    added pubsub event hooks

--- lib/fire_hydrant/jacks/oauth_pubsub_jack.rb
+++ lib/fire_hydrant/jacks/oauth_pubsub_jack.rb
@@ -6,10 +6,18 @@ class OAuthPubSubJack
       @oauth_consumer = OAuth::Consumer.new(@config[:oauth_consumer_key], @config[:oauth_consumer_secret])
       @oauth_token = OAuth::Token.new(@config[:oauth_token], @config[:oauth_token_secret])
       @general_token = OAuth::Token.new(@config[:general_token], @config[:general_token_secret])
+
+      @pubsub.add_event_callback do |event|
+        on(:pubsub_event, event)
+      end
     end
 
     def switchboard.pubsub
       @pubsub
+    end
+
+    def switchboard.on_pubsub_event(&block)
+      register_hook(:pubsub_event, &block)
     end
   end
 end
