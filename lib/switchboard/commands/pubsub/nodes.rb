8ef0ab8d71046d251d5a16e25671ccafc21a5148
Seth Fitzsimmons <seth@mojodna.net>
2008-11-16 21:43:22 -0800

    pubsub nodes command for listing available nodes (broken in xmpp4r-0.4.0)

---
+++ lib/switchboard/commands/pubsub/nodes.rb
@@ -0,0 +1,41 @@
+module Switchboard
+  module Commands
+    class PubSub
+      class Nodes < Switchboard::Command
+        hide! # Jabber::PubSub::NodeBrowser is broken in xmpp4r-0.4.0
+        description "Lists available pubsub nodes (use pubsub.<server>)"
+
+        def self.run!
+          switchboard = Switchboard::Core.new do
+            defer :nodes_retrieved do
+              begin
+                browser = Jabber::PubSub::NodeBrowser.new(client)
+                browser.nodes(OPTIONS["pubsub.server"])
+              rescue Jabber::ServerError => e
+                puts e
+              end
+            end
+
+            def nodes_retrieved(nodes)
+              if nodes && nodes.compact! && nodes.any?
+                puts "Nodes available on '#{OPTIONS["pubsub.server"]}':"
+                nodes.each do |node|
+                  puts "  #{node.to_s}"
+                end
+              else
+                puts "No nodes are available on '#{OPTIONS["pubsub.server"]}'."
+              end
+            end
+          end
+
+          if OPTIONS["oauth"]
+            switchboard.plug!(OAuthPubSubJack)
+          else
+            switchboard.plug!(PubSubJack)
+          end
+          switchboard.run!
+        end
+      end
+    end
+  end
+end
