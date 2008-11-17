039b02b1e0c5f6abfcfa21e4a5040e1d5f88d21f
Seth Fitzsimmons <seth@mojodna.net>
2008-11-16 18:55:33 -0800

    command for creating pubsub nodes

--- lib/switchboard/commands/pubsub.rb
+++ lib/switchboard/commands/pubsub.rb
@@ -1,5 +1,6 @@
 require 'switchboard/commands/pubsub/pubsub'
 require 'switchboard/commands/pubsub/affiliations'
+require 'switchboard/commands/pubsub/create'
 require 'switchboard/commands/pubsub/subscribe'
 require 'switchboard/commands/pubsub/subscriptions'
 require 'switchboard/commands/pubsub/unsubscribe'
---
+++ lib/switchboard/commands/pubsub/create.rb
@@ -0,0 +1,36 @@
+module Switchboard
+  module Commands
+    class PubSub
+      class Create < Switchboard::Command
+        description "Creates a pubsub node"
+
+        def self.run!
+          switchboard = Switchboard::Core.new do
+            defer :node_created do
+              begin
+                pubsub.create_node(OPTIONS["pubsub.node"])
+              rescue Jabber::ServerError => e
+                puts e
+              end
+            end
+
+            def node_created(name)
+              if name
+                puts "Node #{name} was created."
+              else
+                puts "An auto-named node was created."
+              end
+            end
+          end
+
+          if OPTIONS["oauth"]
+            switchboard.plug!(OAuthPubSubJack)
+          else
+            switchboard.plug!(PubSubJack)
+          end
+          switchboard.run!
+        end
+      end
+    end
+  end
+end
